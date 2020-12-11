70f101f49f82d77c2649afa28b27302d094147c8
Spacial <spacial@gmail.com>
2020-12-10 23:53:36 -0300

    day 03 and 04 ok

--- d03/day03.py
+++ d03/day03.py
@@ -11,79 +11,28 @@ def mark(n, c, line):
     return ''.join(lst)
 
 
-def mostraMap(mapa):
-    for i in mapa:
-        print(i)
-
-
-def multimap(mapa):
-    k = (len(mapa) / (len(mapa[0])) ) * 3
-    # print("multiplying map by ", k)
-    newMap = []
-    for i in mapa:
-        newline = i*(int(k)+1)
-        newMap.append(newline)
-    return newMap
-
-
-def count_trees(yslope, xslope, mapa):
-    width = len(mapa[0])
-    height = len(mapa)
-
-    pos = [0, 0]
+def part1(xslope, yslope, mapa):
     trees = 0
-    while pos[0] < height:
-        trees += mapa[pos[0]][pos[1]] == "#"
-        pos[1] = (pos[1] + xslope) % width
-        pos[0] += yslope
-
-    return trees
-
-
-def part1(mapa):
-    trees = 0
-    y = 2
-    # novomapa = []
-    # novomapa.append(mapa[0])
-    # nope = 1
-    # init = True
-    # print(len(mapa[0]), len(mapa))
-    for i in range(1, len(mapa)):
-        # log = '   | ' + str(i)+':'+str(y)
-        y = y % (len(mapa[0]))
-        # try:
-        #   log += '='+str(mapa[i][y])
-        # except:
-        #     print(i)
-        #     print(y)
-        #     print(mapa[i])
-        #     sys.exit(1)
-        if mapa[i][y] == '#':
+    x = xslope
+    for y in range(yslope, len(mapa), yslope):
+        x = x % (len(mapa[0]) )
+        if mapa[y][x] == '#':
             trees += 1
-        #     mapa[i] = mark(y, 'X', mapa[i])
-        # else:
-        #     mapa[i] = mark(y, 'O', mapa[i])
-        #     nope += 1
-        # log += '->'+str(mapa[i][y])+'|'+str(y)
-        # novomapa.append(mapa[i]+log)
-        y += 3
-    # mostraMap(novomapa)
-    # print('nopes:', nope)
-    # print(len(mapa[0]))
-    # if trees+nope == len(mapa):
-    #     print("ok")
-    # else:
-    #     print(trees+nope, len(mapa))
+            mapa[y] = mark(x, 'X', mapa[y])
+        else:
+            mapa[y] = mark(x, 'O', mapa[y])
+        x += xslope
     return trees
-
-
-def part2():
-    return
-
 
 entries = []
 for line in sys.stdin:
     entries.append(line.strip('\n'))
 
-print(part1(entries))
-print(count_trees(1, 3, entries))
+# print(part1(3, 1, entries))
+slopes = [ [1,1], [3,1], [5,1], [7,1], [1,2] ]
+k = 1
+for i in slopes:
+    x, y = i
+    ecopy = entries.copy()
+    k *= part1(x, y, ecopy)
+print(k)
---
+++ d04/day4.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# License: GPL3
+# Day N
+
+import sys
+import re
+
+
+def part1(data):
+    requiredfields = ['byr', 'ecl', 'eyr', 'hcl', 'hgt', 'iyr', 'pid']
+    valids = 0
+    for i in data:
+        thisfields = 0
+        for f in i:
+            if f in requiredfields:
+                thisfields += 1
+        if thisfields == len(requiredfields):
+            valids += 1
+    return valids
+
+
+def validateField(field, data):
+    hclRegex = "^#[A-Fa-f0-9]{6}"
+    p = re.compile(hclRegex)
+    byr = [1920, 2002]
+    iyr = [2010, 2020]
+    eyr = [2020, 2030]
+    hgtcm = [150, 193]
+    hgtin = [59, 76]
+    ecl = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+    pid = 9
+    if field in ['byr', 'iyr', 'eyr', 'hgt']:
+        if field == 'byr':
+            if int(data) < byr[0] or int(data) > byr[1]:
+                # print('byr:0,', end='')
+                return False
+        if field == 'iyr':
+            if int(data) < iyr[0] or int(data) > iyr[1]:
+                # print('iyr:0,', end='')
+                return False
+        if field == 'eyr':
+            if int(data) < eyr[0] or int(data) > eyr[1]:
+                # print(field, data, eyr)
+                # print('eyr:0,', end='')
+                return False
+        if field == 'hgt':
+            size = data.split('c')[0]
+            if len(data.split('c')) > 1:
+                if hgtcm[0] <= int(size) <= hgtcm[1]:
+                    return True
+            size = data.split('i')[0]
+            if len(data.split('i')) > 1:
+                if hgtin[0] <= int(size) <= hgtin[1]:
+                    return True
+            return False
+    elif field == 'ecl':
+        if data not in ecl:
+            # print('ecl:0,', end='')
+            return False
+    elif field == 'hcl':
+        if not (re.search(p, data)):
+            # print('hcl:0,', end='')
+            return False
+    elif field == 'pid':
+        if len(data) != pid and type(data) != int:
+            # print('pid:0,', end='')
+            return False
+    return True
+
+
+def part2(data):
+    valids = 0
+    for person in data:
+        thisfields = 0
+        for f in person:
+            if validateField(f, person[f]):
+                thisfields += 1
+        if thisfields == len(person):
+            valids += 1
+    return valids
+
+
+lst = sys.stdin.read().split('\n\n')
+lst = [x.replace('\n', ' ').split() for x in lst]
+passports = []
+for person in lst:
+    passports.append(dict(data.split(':') for data in person))
+
+print(part1(passports))
+
+passports = [x for x in passports if len(x.keys()) == 8 or (len(x) == 7 and 'cid' not in x.keys())]
+
+print(part2(passports))
